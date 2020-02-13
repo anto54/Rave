b7e8b79d91f30737bf7195a554e368900c439487
anthony <laurentanthony54220@gmail.com>
2020-02-13 16:33:58 +0100

    commit

--- java.js
+++ java.js
@@ -1,37 +1,32 @@
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModalRoasterRL");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtnRoasterRL");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 
 // Get the modal
-var modal = document.getElementById("myModal1");
+var modal1 = document.getElementById("myModalRoasterR6");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn1");
+var myBtnRoasterR6 = document.getElementById("myBtnRoasterR6");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+myBtnRoasterR6.onclick = function() {
+  modal1.style.display = "block";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if ((event.target == modal1) || (event.target == modal)) {
+    modal1.style.display = "none";
     modal.style.display = "none";
   }
 }
-
